6204315d2fb04d16af8479623687ddefce807eee
Matthew Rogers <matt@matthewrogers.org>
2024-02-21 21:39:30 -0500

    bigger text

--- getWeather.mjs
+++ getWeather.mjs
@@ -10,15 +10,12 @@ async function getWeather(){
   let output = "";
 
   //write out date
-  //console.log(new Date().toString());
   output += (new Date().toString()) + "\n<br>";
 
   //write out alerts if they exist
   const alerts = await client.getAlerts(active, { latitude, longitude });
   alerts.features.forEach(feature => {
-    console.log(feature.properties.description);
     output += "ALERTS! : " + feature.properties.description  + "\n<br>";
-    //console.log(feature.geometry);
     output += feature.geometry + "\n";
   });
 
@@ -27,22 +24,17 @@ async function getWeather(){
   if (nearestStation) {
     const { stationIdentifier } = nearestStation.properties;
     const latestObservation = await client.getLatestStationObservations(stationIdentifier);
-    //console.log(latestObservation.properties.temperature);
-    var temperature = latestObservation.properties.temperature.value * 9 /5 + 32
-    //console.log("Current Temperature: " + temperature + "F")
+    var temperature = Math.round(latestObservation.properties.temperature.value * 9 /5 + 32)
     output += "<h3>Current Temperature: " + temperature + "F" + "🌡️\n</h3>";
-
+    output += '<div id="topRightDiv">'+  temperature + "F" + "🌡️" +'</div>';
   }
-
 
   const forecast = await client.getForecast(latitude, longitude, 'baseline');
   forecast.properties.periods.forEach(period => {
-    //console.log(`${period.name}: ${period.detailedForecast}`);
     output += `<p>${period.name}: ${period.detailedForecast}  \n</p>`;
   });
 
   return output;
 }
 
-//console.log(await getWeather())
 export { getWeather };
