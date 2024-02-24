14aa638282d29461ec6c71566ccc00c24cd3aed0
RamboRogers <matt@matthewrogers.org>
2024-02-24 13:56:14 -0500

    Fixed error on internet failure causing crash.

--- getWeather.mjs
+++ getWeather.mjs
@@ -11,30 +11,38 @@ async function getWeather(){
 
   //write out date
   output += (new Date().toString()) + "\n<br>";
+  try {
+    
+    //write out alerts if they exist
+    const alerts = await client.getAlerts(active, { latitude, longitude });
+    alerts.features.forEach(feature => {
+      output += "ALERTS! : " + feature.properties.description  + "\n<br>";
+      output += feature.geometry + "\n";
+    });
 
-  //write out alerts if they exist
-  const alerts = await client.getAlerts(active, { latitude, longitude });
-  alerts.features.forEach(feature => {
-    output += "ALERTS! : " + feature.properties.description  + "\n<br>";
-    output += feature.geometry + "\n";
-  });
+    //write out current temperature
+    const nearestStation = await client.getNearestStation(latitude, longitude);
+    if (nearestStation) {
+      const { stationIdentifier } = nearestStation.properties;
+      const latestObservation = await client.getLatestStationObservations(stationIdentifier);
+      var temperature = Math.round(latestObservation.properties.temperature.value * 9 /5 + 32)
+      output += "<h3>Current Temperature: " + temperature + "F" + "🌡️\n</h3>";
+      output += '<div id="topRightDiv">'+  temperature + "F" + "🌡️" +'</div>';
+    }
 
-  //write out current temperature
-  const nearestStation = await client.getNearestStation(latitude, longitude);
-  if (nearestStation) {
-    const { stationIdentifier } = nearestStation.properties;
-    const latestObservation = await client.getLatestStationObservations(stationIdentifier);
-    var temperature = Math.round(latestObservation.properties.temperature.value * 9 /5 + 32)
-    output += "<h3>Current Temperature: " + temperature + "F" + "🌡️\n</h3>";
-    output += '<div id="topRightDiv">'+  temperature + "F" + "🌡️" +'</div>';
-  }
+    const forecast = await client.getForecast(latitude, longitude, 'baseline');
+    forecast.properties.periods.forEach(period => {
+      output += `<p>${period.name}: ${period.detailedForecast}  \n</p>`;
+    });
 
-  const forecast = await client.getForecast(latitude, longitude, 'baseline');
-  forecast.properties.periods.forEach(period => {
-    output += `<p>${period.name}: ${period.detailedForecast}  \n</p>`;
-  });
+    return output;
 
-  return output;
+} catch (error) {
+    
+  return (new Date().toString()) + "\n<br><h1>Error:" + error.value + "</h1>"
+}
+
+  
 }
 
 export { getWeather };
